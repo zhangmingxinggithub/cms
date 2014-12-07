0bec8df1fc0006833201044360702d30a5441eac
zhangmingxinggithub <1205317132@qq.com>
2014-12-07 16:12:09 +0800

    厕所

--- src/Test.java
+++ src/Test.java
@@ -4,7 +4,7 @@ public class Test {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
           System.out.println("tes4444444ttesttest");
-          System.out.println("欧阳利测试");
+          System.out.println("欧阳利是个傻逼");
 	}
 
 }
