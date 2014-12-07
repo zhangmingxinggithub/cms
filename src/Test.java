70f9c91328ec47fcc2393e40c521cd5995b7ddf1
zhangmingxinggithub <1205317132@qq.com>
2014-12-07 16:46:32 +0800

    test

--- src/Test.java
+++ src/Test.java
@@ -3,13 +3,7 @@ public class Test {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-          System.out.println("tes4444444ttesttest");
-          System.out.println("欧阳利是个傻逼");
-          System.out.println("张明星是个大大的傻逼");
-          
-          System.out.println("张明星是个大大的傻逼");
-          System.out.println("张明星是个大大的傻逼");
-          System.out.println("张明星是个大大的傻逼111");
+          System.out.println("欧阳还是处男 哈哈");
 	}
 
 }
