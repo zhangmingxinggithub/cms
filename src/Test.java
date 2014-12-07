07cb59d274332a36b9edb6c907345bcd58d45b7f
zhangmingxinggithub <1205317132@qq.com>
2014-12-07 16:55:42 +0800

    tt

--- src/Test.java
+++ src/Test.java
@@ -4,6 +4,8 @@ public class Test {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
           System.out.println("欧阳还是处男 哈哈");
+          
+          System.out.println("明显还是 哈哈");
 	}
 
 }
