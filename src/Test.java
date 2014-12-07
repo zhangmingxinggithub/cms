36d1d7c6f208ea89e42eed9653a5e0f054432a6d
zhangmingxinggithub <1205317132@qq.com>
2014-12-07 16:06:30 +0800

    测试

--- src/Test.java
+++ src/Test.java
@@ -4,6 +4,7 @@ public class Test {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
           System.out.println("tes4444444ttesttest");
+          System.out.println("欧阳利测试");
 	}
 
 }
